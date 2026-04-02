0c67ef811fdf82d248c22a64f93974ac5ec30782
davidheineman <david@davidheineman.com>
2026-04-02 12:30:55 -0700

    sets 3 and 4!

--- constants.py
+++ constants.py
@@ -131,8 +131,6 @@ TASK_MAP_SET_1 = [
     },
 ]
 
-
-""" Commented-out tasks have parity! """
 TASK_MAP_SET_2 = [
     {
         "old_tasks": [
@@ -170,93 +168,442 @@ TASK_MAP_SET_2 = [
     },
 ]
 
-
-# The datalake did not save example queries for these tasks
-TASK_MAP_NO_EXAMPLES = [
+# These are not yet implemented
+TASK_MAP_SET_3 = [
+    {
+        "old_tasks": [
+            "coqa:mc::gen2mc:xlarge",
+        ],
+        "new_tasks": [
+            "coqa:mc::olmo3base",
+        ]
+    },
+    {
+        "old_tasks": [
+            "drop:mc::gen2mc:xlarge",
+        ],
+        "new_tasks": [
+            "drop:mc::olmo3base",
+        ]
+    },
+    {
+        "old_tasks": [
+            "jeopardy:mc::gen2mc:xlarge",
+        ],
+        "new_tasks": [
+            "jeopardy:mc::olmo3base",
+        ]
+    },
+    {
+        "old_tasks": [
+            "naturalqs:mc::gen2mc:xlarge",
+        ],
+        "new_tasks": [
+            "naturalqs:mc::olmo3base",
+        ]
+    },
+    {
+        "old_tasks": [
+            "squad:mc::gen2mc:xlarge",
+        ],
+        "new_tasks": [
+            "squad:mc::olmo3base",
+        ]
+    },
     {
         "old_tasks": [
             "winogrande:rc::xlarge",
         ],
         "new_tasks": [
             "winogrande:rc::olmo3base",
-        ],
-    },
-    {
-        "old_tasks": [
-            "naturalqs::xlarge",
-            "naturalqs:mc::gen2mc:xlarge",
-            "naturalqs:rc::gen2mc:xlarge",
-        ],
-        "new_tasks": [
-            "naturalqs:gen::olmo3base",
-            "naturalqs:mc::olmo3base",
-            "naturalqs:rc::olmo3base",
-        ],
+        ]
     },
     {
         "old_tasks": [
             "basic_skills_arithmetic:rc::olmes",
-            "basic_skills_coding:rc::olmes",
-            "basic_skills_common_knowledge:rc::olmes",
-            "basic_skills_logical_reasoning:rc::olmes",
-            "basic_skills_pattern:rc::olmes",
-            "basic_skills_string_operations:rc::olmes",
         ],
         "new_tasks": [
             "basic_skills_arithmetic:rc::olmo3base",
+        ]
+    },
+    {
+        "old_tasks": [
+            "basic_skills_coding:rc::olmes",
+        ],
+        "new_tasks": [
             "basic_skills_coding:rc::olmo3base",
+        ]
+    },
+    {
+        "old_tasks": [
+            "basic_skills_common_knowledge:rc::olmes",
+        ],
+        "new_tasks": [
             "basic_skills_common_knowledge:rc::olmo3base",
+        ]
+    },
+    {
+        "old_tasks": [
+            "basic_skills_logical_reasoning:rc::olmes",
+        ],
+        "new_tasks": [
             "basic_skills_logical_reasoning:rc::olmo3base",
-            "basic_skills_pattern:rc::olmo3base",
+        ]
+    },
+    {
+        "old_tasks": [
+            "basic_skills_string_operations:rc::olmes",
+        ],
+        "new_tasks": [
             "basic_skills_string_operations:rc::olmo3base",
+        ]
+    },
+    {
+        "old_tasks": [
+            "basic_skills_pattern:rc::olmes",
+        ],
+        "new_tasks": [
+            "basic_skills_pattern:rc::olmo3base",
+        ]
+    },
+    {
+        "old_tasks": [
+            "gsm8k::olmo3:n8:v2",
+        ],
+        "new_tasks": [
+            "gsm8k::olmo3base",
+        ]
+    },
+    {
+        "old_tasks": [
+            "gsm_symbolic::olmo3:n8:v2",
+        ],
+        "new_tasks": [
+            "gsm_symbolic::olmo3base",
+        ]
+    },
+    {
+        "old_tasks": [
+            "gsm_symbolic:p1::olmo3:n8:v2",
+        ],
+        "new_tasks": [
+            "gsm_symbolic:p1::olmo3base",
+        ]
+    },
+    {
+        "old_tasks": [
+            "gsm_symbolic:p2::olmo3:n8:v2",
+        ],
+        "new_tasks": [
+            "gsm_symbolic:p2::olmo3base",
+        ]
+    },
+    {
+        "old_tasks": [
+            "minerva_math_algebra::olmes:n4:v2",
+        ],
+        "new_tasks": [
+            "minerva_math_algebra::olmo3base",
+        ]
+    },
+    {
+        "old_tasks": [
+            "minerva_math_counting_and_probability::olmes:n4:v2",
+        ],
+        "new_tasks": [
+            "minerva_math_counting_and_probability::olmo3base",
+        ]
+    },
+    {
+        "old_tasks": [
+            "minerva_math_geometry::olmes:n4:v2",
+        ],
+        "new_tasks": [
+            "minerva_math_geometry::olmo3base",
+        ]
+    },
+    {
+        "old_tasks": [
+            "minerva_math_intermediate_algebra::olmes:n4:v2",
+        ],
+        "new_tasks": [
+            "minerva_math_intermediate_algebra::olmo3base",
+        ]
+    },
+    {
+        "old_tasks": [
+            "minerva_math_number_theory::olmes:n4:v2",
+        ],
+        "new_tasks": [
+            "minerva_math_number_theory::olmo3base",
+        ]
+    },
+    {
+        "old_tasks": [
+            "minerva_math_prealgebra::olmes:n4:v2",
+        ],
+        "new_tasks": [
+            "minerva_math_prealgebra::olmo3base",
+        ]
+    },
+    {
+        "old_tasks": [
+            "minerva_math_precalculus::olmes:n4:v2",
+        ],
+        "new_tasks": [
+            "minerva_math_precalculus::olmo3base",
+        ]
+    },
+]
+
+# This set should be implemented, but may not have RC task configs
+TASK_MAP_SET_4 = [
+    {
+        "old_tasks": [
+            "arc_challenge:rc::xlarge"
+        ], 
+        "new_tasks": [
+            "arc_challenge:rc::olmo3base",
         ],
     },
     {
         "old_tasks": [
-            "coqa:mc::gen2mc:xlarge",
-            "coqa:rc::gen2mc:xlarge",
-        ],
+            "arc_easy:rc::xlarge"
+        ], 
         "new_tasks": [
-            "coqa:mc::olmo3base",
+            "arc_easy:rc::olmo3base",
+        ],
+    },
+    {
+        "old_tasks": [
+            "mmlu_stem:rc"
+        ], 
+        "new_tasks": [
+            "mmlu:stem:rc::olmo3base",
+        ],
+    },
+    {
+        "old_tasks": [
+            "mmlu_humanities:rc"
+        ], 
+        "new_tasks": [
+            "mmlu:humanities:rc::olmo3base",
+        ],
+    },
+    {
+        "old_tasks": [
+            "mmlu_other:rc"
+        ], 
+        "new_tasks": [
+            "mmlu:other:rc::olmo3base",
+        ],
+    },
+    {
+        "old_tasks": [
+            "mmlu_social_sciences:mc"
+        ], 
+        "new_tasks": [
+            "mmlu:social_sciences:rc::olmo3base",
+        ],
+    },
+    {
+        "old_tasks": [
+            "csqa:rc::olmes:full"
+        ], 
+        "new_tasks": [
+            "csqa:rc::olmo3base",
+        ],
+    },
+    {
+        "old_tasks": [
+            "hellaswag:rc::olmes:full"
+        ], 
+        "new_tasks": [
+            "hellaswag:rc::olmo3base",
+        ],
+    },
+    {
+        "old_tasks": [
+            "winogrande:rc::olmes:full"
+        ], 
+        "new_tasks": [
+            "winogrande:rc::olmo3base",
+        ],
+    },
+    {
+        "old_tasks": [
+            "socialiqa:rc::olmes:full"
+        ], 
+        "new_tasks": [
+            "socialiqa:rc::olmo3base",
+        ],
+    },
+    {
+        "old_tasks": [
+            "piqa:rc::olmes:full"
+        ], 
+        "new_tasks": [
+            "piqa:rc::olmo3base",
+        ],
+    },
+    {
+        "old_tasks": [
+            "coqa:rc::gen2mc:xlarge"
+        ], 
+        "new_tasks": [
             "coqa:rc::olmo3base",
         ],
     },
     {
         "old_tasks": [
-            "drop:mc::gen2mc:xlarge",
-            "drop:rc::gen2mc:xlarge",
-        ],
+            "drop:rc::gen2mc:xlarge"
+        ], 
         "new_tasks": [
-            "drop:mc::olmo3base",
             "drop:rc::olmo3base",
         ],
     },
     {
         "old_tasks": [
-            "jeopardy:mc::gen2mc:xlarge",
-            "jeopardy:rc::gen2mc:xlarge",
-        ],
+            "jeopardy:rc::gen2mc:xlarge"
+        ], 
         "new_tasks": [
-            "jeopardy:mc::olmo3base",
             "jeopardy:rc::olmo3base",
         ],
     },
     {
         "old_tasks": [
-            "sciq:rc::olmo3",
+            "naturalqs:rc::gen2mc:xlarge"
+        ], 
+        "new_tasks": [
+            "naturalqs:rc::olmo3base",
         ],
+    },
+    {
+        "old_tasks": [
+            "squad:rc::gen2mc:xlarge"
+        ], 
+        "new_tasks": [
+            "squad:rc::olmo3base",
+        ],
+    },
+    {
+        "old_tasks": [
+            "sciq:rc::olmo3"
+        ], 
         "new_tasks": [
             "sciq:rc::olmo3base",
         ],
     },
     {
         "old_tasks": [
-            "squad:mc::gen2mc:xlarge",
-            "squad:rc::gen2mc:xlarge",
-        ],
+            "qasper_yesno:rc::olmes"
+        ], 
         "new_tasks": [
-            "squad:mc::olmo3base",
-            "squad:rc::olmo3base",
+            "qasper_yesno:rc::olmo3base",
         ],
     },
+    {
+        "old_tasks": [
+            "basic_skills_arithmetic:rc::olmes"
+        ], 
+        "new_tasks": [
+            "basic_skills_arithmetic:rc::olmo3base",
+        ],
+    },
+    {
+        "old_tasks": [
+            "basic_skills_coding:rc::olmes"
+        ], 
+        "new_tasks": [
+            "basic_skills_coding:rc::olmo3base",
+        ],
+    },
+    {
+        "old_tasks": [
+            "basic_skills_common_knowledge:rc::olmes"
+        ], 
+        "new_tasks": [
+            "basic_skills_common_knowledge:rc::olmo3base",
+        ],
+    },
+    {
+        "old_tasks": [
+            "basic_skills_logical_reasoning:rc::olmes"
+        ], 
+        "new_tasks": [
+            "basic_skills_logical_reasoning:rc::olmo3base",
+        ],
+    },
+    {
+        "old_tasks": [
+            "basic_skills_string_operations:rc::olmes"
+        ], 
+        "new_tasks": [
+            "basic_skills_string_operations:rc::olmo3base",
+        ],
+    },
+    {
+        "old_tasks": [
+            "basic_skills_pattern:rc::olmes"
+        ], 
+        "new_tasks": [
+            "basic_skills_pattern:rc::olmo3base",
+        ],
+    },
+    {
+        "old_tasks": [
+            "lab_bench_dbqa"
+        ], 
+        "new_tasks": [
+            "lab_bench_dbqa::olmo3base",
+        ],
+    },
+    {
+        "old_tasks": [
+            "lab_bench_protocolqa"
+        ], 
+        "new_tasks": [
+            "lab_bench_protocolqa::olmo3base",
+        ],
+    },
+    {
+        "old_tasks": [
+            "lambada"
+        ], 
+        "new_tasks": [
+            "lambada",
+        ],
+    },
+    {
+        "old_tasks": [
+            "medmcqa:rc::none"
+        ], 
+        "new_tasks": [
+            "medmcqa:rc::olmo3base",
+        ],
+    },
+    {
+        "old_tasks": [
+            "medqa_en:rc::none"
+        ], 
+        "new_tasks": [
+            "medqa_en:rc::olmo3base",
+        ],
+    },
+    {
+        "old_tasks": [
+            "sciriff_yesno:rc::olmes"
+        ], 
+        "new_tasks": [
+            "sciriff_yesno:rc::olmo3base",
+        ],
+    }
+]
+
+TASK_MAP_SET_5 = [
+    # This will have QA and Math BPB sets.
+]
+
+TASK_MAP_SET_6 = [
+    # This will have Code Gen sets (should be more challenging b/c of execution)
 ]
